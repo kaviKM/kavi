aae75875639c636e997357b744322d7ac07fb035
kaviKM <kavibharathi9626@gmail.com>
2017-09-18 12:30:55 +0530

    Create exp_9.c

---
+++ exp_9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,i=1, sum=0;
+printf("\n Enter the value of N:");
+scanf("%d",&n);
+printf("\n First %d numbers are\n",n);
+for(i=1;i<=n;i++)
+{
+printf("%7d",i);
+Sum=sum+i;
+}
+printf("Sum=%d,sum);
+return 0;
+}
+
